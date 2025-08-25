9c06c4a47cf7d3c93ae57b17c6d766c93f7c8d5b
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-08-25 14:03:23 +0530

    improved Navbar

--- Frontend/src/views/includes/Navbar.jsx
+++ Frontend/src/views/includes/Navbar.jsx
@@ -1,9 +1,8 @@
-
 function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg border-bottom border-dark py-3 fs-5 ">
-        <div className="container-fluid mx-3 ml-4">
+        <div className="container-fluid mx-3 ml-4 " >
           <a className="navbar-brand fs-3" style={{color:"#0D6EFD"}} href="#"><i class="fa-solid fa-otter"></i>Think-Together</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -22,20 +21,22 @@ function Navbar() {
               <li className="nav-item">
                 <a className="nav-link" href="#">About</a> 
               </li>
+            </ul>
+            <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a className="nav-link" href="#"><button className="btn btn-primary">Post Idea</button></a>
               </li>
-              {/* <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown link
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li> */}
             </ul>
+            {/* <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown link
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li> */}
           </div>
         </div>
       </nav>
