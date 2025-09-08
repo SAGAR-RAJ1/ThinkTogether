de87a01666f0185302e12771a4e7cea1944bc484
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-09-08 10:21:07 +0530

    fetch ideas from backend and display them in cards; update Idea component to accept title prop

--- Frontend/src/components/Cards/Cards.jsx
+++ Frontend/src/components/Cards/Cards.jsx
@@ -1,29 +1,46 @@
-import "./Cards.css"
-import Idea from "../Idea/Idea"
+import "./Cards.css";
+import Idea from "../Idea/Idea";
+import { useEffect, useState } from "react";
+
 export default function Cards() {
+  const [ideas, setIdeas] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/ideas")
+      .then((res) => res.json())
+      .then((data) => setIdeas(data))
+      .catch((err) => console.error("Error fetching ideas:", err));
+  }, []);
+
   return (
-  <>
-     <div className="container mt-5 " >
-        <h2 className="mb-5"  id="ideaCont">The Featured Cards</h2>
+    <>
+      <div className="container mt-5 ">
+        <h2 className="mb-5" id="ideaCont">
+          The Featured Cards
+        </h2>
 
-        <hr /><br /><br />
+        <hr />
+        <br />
+        <br />
         <div className="ideas">
-
-
-        <div className="d-flex flex-wrap gap-4   ">
-  <Idea link="./images/img.png"  desc="lore dkb ilbgeuwg ufduewug ucviwef7frgbxoug2oknkw bfii dfguofouuofubckubekuhvcueyvvcrogougbfougkvckiuefroyfgkehfku giu"/>
-  <Idea link="./images/img.png" desc="hy" />
-  <Idea link="./images/src.jpeg" desc="bfvufudcudvjfkkuyfkufjydtsrharstdkfklufkfyfjdvtrsdusuluvgluyulyfudfkydrtsdhrsqwertyuiokjnbvcxswer5678uikjnbvcder567uikj " />
-  <Idea link="./images/src.jpeg" />
-  <Idea link="./images/src.jpeg" />
-  <Idea link="./images/src.jpeg" />
-  <Idea link="./images/img.png" />
-
-  <Idea link="./images/img.png" />
-
-</div>
+          <div className="d-flex flex-wrap gap-4">
+            {ideas.length === 0 ? (
+              <p>Loading ideas...</p>
+            ) : (
+              ideas
+                .slice(0, 8)
+                .map((idea) => (
+                  <Idea
+                    key={idea._id}
+                    link={idea.image.url}
+                    title={idea.title}
+                    desc={idea.description}
+                  />
+                ))
+            )}
+          </div>
+          {/* <Idea link="./images/img.png" title="sagar" desc="lore dkb ilbgeuwg ufduewug ucviwef7frgbxoug2oknkw bfii dfguofouuofubckubekuhvcueyvvcrogougbfougkvckiuefroyfgkehfku giu"/> */}
         </div>
-     </div>
-  </>
-  )
+      </div>
+    </>
+  );
 }
--- Frontend/src/components/Idea/Idea.jsx
+++ Frontend/src/components/Idea/Idea.jsx
@@ -1,12 +1,12 @@
 
 import "./Idea.css"
-export default function Idea({link,desc}) {
+export default function Idea({link,desc,title}) {
   return (
     <div>
    <div className="card border-2" style={{ width: "18rem" }}>
   <img src={link} className="card-img-top" alt="..." />
   <div className="card-body">
-    <h5 className="card-title">Card title</h5>
+    <h5 className="card-title">{title}</h5>
     <p className="card-text">{desc}</p>
     <a href="#" className="btn btn-primary">Explore</a>
   </div>
