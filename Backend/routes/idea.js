55266badb547bd392c8b61dddcb70e1039d6d7da
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-24 11:05:56 +0530

    Added loggedIn middleware to the routes

--- Backend/routes/idea.js
+++ Backend/routes/idea.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Idea = require("../models/Idea.js");
 const router = express.Router();
+const {isLoggedIn}=require("../Middleware/script.js")
 
 
 const IdeaController = require("../controllers/ideas.js");
@@ -8,9 +9,9 @@ const IdeaController = require("../controllers/ideas.js");
 // Get all ideas
 router.get("/", IdeaController.getAllIdeas);
 // Create a new idea
-router.post("/", IdeaController.createIdea);
+router.post("/",isLoggedIn, IdeaController.createIdea);
 //Delete an idea
-router.delete("/:id", IdeaController.deleteIdea);
+router.delete("/:id",isLoggedIn, IdeaController.deleteIdea);
 
 
 
