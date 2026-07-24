497dd15a02dffa2596953d4822afad7b2f53b911
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-24 21:28:12 +0530

    Made the navbar good af

--- Frontend/src/views/includes/Navbar.jsx
+++ Frontend/src/views/includes/Navbar.jsx
@@ -1,12 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
-import "./Includes.css";
 import { Dropdown } from "react-bootstrap";
+import "./Includes.css";
+
 function Navbar() {
   const navigate = useNavigate();
   const API = import.meta.env.VITE_API_URL;
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get(`${API}/user/profile`, {
+          withCredentials: true,
+        });
+
+        setUser(res.data.user);
+      } catch (err) {
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+  }, []);
 
   const handleLogout = async () => {
     try {
@@ -19,7 +38,7 @@ function Navbar() {
       );
 
       toast.success("Logged out successfully");
-
+      setUser(null);
       navigate("/");
     } catch (err) {
       console.error(err);
@@ -37,9 +56,9 @@ function Navbar() {
         background: "transparent",
       }}
     >
-      <nav className="navbar navbar-expand-lg border-bottom border-dark py-3 fs-5 ">
+      <nav className="navbar navbar-expand-lg border-bottom border-dark py-3 fs-5">
         <div
-          className="container-fluid mx-3 ml-4  .navbar"
+          className="container-fluid mx-3"
           style={{ background: "transparent" }}
         >
           <Link
@@ -48,87 +67,88 @@ function Navbar() {
             to="/"
           >
             <i
+              className="fa-solid fa-otter"
               style={{ background: "transparent" }}
-              class="fa-solid fa-otter"
-            ></i>
+            ></i>{" "}
             Think-Together
           </Link>
+
           <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
+
           <div
             className="collapse navbar-collapse"
             id="navbarNavDropdown"
             style={{ background: "transparent" }}
           >
             <ul className="navbar-nav" style={{ background: "transparent" }}>
-              <li className="nav-item" style={{ background: "transparent" }}>
-                <Link className="nav-link active" aria-current="page" to="/">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
                   Home
                 </Link>
               </li>
-              <li className="nav-item" style={{ background: "transparent" }}>
+
+              <li className="nav-item">
                 <Link className="nav-link" to="/ideas">
                   Ideas
                 </Link>
               </li>
-              <li className="nav-item" style={{ background: "transparent" }}>
-                <Link
-                  className="nav-link"
-                  to="/myIdeas"
-                  smooth={true}
-                  duration={500}
-                >
+
+              <li className="nav-item">
+                <Link className="nav-link" to="/myIdeas">
                   My Ideas
                 </Link>
               </li>
-              {/* <li className="nav-item"  style={{background:"transparent"}}>
-                <Link
-                  className="nav-link"
-                  to="/login"
-                  smooth={true}
-                  duration={500}
-                >
-                  Log In
-                </Link>
-              </li>
-              <li className="nav-item"  style={{background:"transparent"}}>
-                <Link
-                  className="nav-link"
-                  to="/signup"
-            
-                >
-                  Sign up
-                </Link>
-              </li> */}
 
-              <Dropdown style={{ background: "transparent" }}>
-                <Dropdown.Toggle variant="link" id="dropdown-basic">
-                  User
+              <Dropdown align="end" className="ms-3">
+                <Dropdown.Toggle
+                  variant="link"
+                  id="dropdown-basic"
+                  className="text-dark text-decoration-none border-0 shadow-none d-flex align-items-center"
+                  style={{
+                    fontSize: "1.1rem",
+                    fontWeight: "500",
+                    background: "transparent",
+                  }}
+                >
+                  <i className="fa-solid fa-circle-user fs-4 me-2"></i>
+                  {user ? user.username : "Account"}
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu>
-                  <Dropdown.Item href="/login">Log In</Dropdown.Item>
-                  <Dropdown.Item href="/signup">Sign Up</Dropdown.Item>
-                  <Dropdown.Item as="button" onClick={handleLogout}>
-                    Logout
-                  </Dropdown.Item>
+                  {user ? (
+                    <>
+                      <Dropdown.Item as={Link} to="/profile">
+                        Profile
+                      </Dropdown.Item>
+
+                      <Dropdown.Item as="button" onClick={handleLogout}>
+                        Logout
+                      </Dropdown.Item>
+                    </>
+                  ) : (
+                    <>
+                      <Dropdown.Item as={Link} to="/login">
+                        Log In
+                      </Dropdown.Item>
+
+                      <Dropdown.Item as={Link} to="/signup">
+                        Sign Up
+                      </Dropdown.Item>
+                    </>
+                  )}
                 </Dropdown.Menu>
               </Dropdown>
             </ul>
-            <ul
-              className="navbar-nav ms-auto"
-              style={{ background: "transparent" }}
-            >
-              <li className="nav-item" style={{ background: "transparent" }}>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
                 <Link className="nav-link" to="/post">
                   <button className="btn btn-primary">Post Idea</button>
                 </Link>
