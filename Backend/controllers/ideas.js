2bbfec5d36966834998949f419fc8eb17c864023
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2026-07-24 11:04:43 +0530

    create idea route corrected

--- Backend/controllers/ideas.js
+++ Backend/controllers/ideas.js
@@ -1,6 +1,5 @@
 const Idea = require("../models/Idea.js");
 
-
 //Get all Ideas
 exports.getAllIdeas = async (req, res) => {
   try {
@@ -13,33 +12,48 @@ exports.getAllIdeas = async (req, res) => {
 
 // Create a new idea
 exports.createIdea = async (req, res) => {
-    try {
-      const { title, description, image } = req.body;
-      const newIdea = new Idea({
-        title,
-        description,
-        ...(image ? { image: { url: image } } : {}), 
-      });
-  
-      await newIdea.save();
-      res.redirect("http://localhost:5173/ideas");
+  console.log("Inside createIdea");
 
-    } catch (err) {
-      res.status(500).json({ error: "Failed to add idea" });
-    }
+  try {
+    const { title, description, image } = req.body;
+
+    const newIdea = new Idea({
+      title,
+      description,
+      ...(image ? { image: { url: image } } : {}),
+    });
+
+    console.log(req.user);
+
+    newIdea.owner = req.user._id;
+
+    await newIdea.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Idea created successfully",
+      idea: newIdea,
+    });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 // Delete an idea
 exports.deleteIdea = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const deletedIdea = await Idea.findByIdAndDelete(id);
-  
-      if (!deletedIdea) {
-        return res.status(404).json({ error: "Idea not found" });
-      }
-  
-      res.json({ message: "Idea deleted successfully" });
-    } catch (err) {
-      res.status(500).json({ error: "Failed to delete idea" });
+  try {
+    const { id } = req.params;
+    const deletedIdea = await Idea.findByIdAndDelete(id);
+
+    if (!deletedIdea) {
+      return res.status(404).json({ error: "Idea not found" });
     }
+
+    res.json({ message: "Idea deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete idea" });
+  }
 };
